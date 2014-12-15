586bfcc579e10e107b30262a7cc97466bcb66187
Michael Whitehead <michael.whitehead@mediaforge.com>
2014-12-15 15:57:07 -0700

    Tracks the mouse better

--- src/om_menu/core.cljs
+++ src/om_menu/core.cljs
@@ -1,15 +1,63 @@
 (ns om-menu.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
    [secretary.core :as secretary :include-macros true :refer [defroute]]
+   [cljs.core.async :as async :refer [>! <! put! chan close!]]
    [goog.events :as events]
-   [goog.history.EventType :as EventType]
+   [goog.history.EventType :as HistoryEventType]
    [om.core :as om :include-macros true]
    [om.dom :as dom :include-macros true]
    [figwheel.client :as fw])
-  (:import goog.History))
+  (:import goog.History
+           [goog.events EventType]))
 
 
 (enable-console-print!)
+
+;;; Events
+
+
+(def classes
+  {:menu-on "show-menu"
+   :menu-off "hide-menu"})
+
+(def keyword->event-type
+  {:mousedown EventType/MOUSEDOWN
+   :mouseup EventType/MOUSEUP
+   :mousemove EventType/MOUSEMOVE
+   :resize EventType/RESIZE})
+
+(defn listen
+  ([el type] (listen el type nil))
+  ([el type f] (listen el type f (chan)))
+  ([el type f out]
+     (events/listen el (keyword->event-type type)
+                    (fn [e] (when f (f e)) (put! out e)))
+     out))
+
+(defn drag [el start-predicate]
+  (let [out (chan)
+        start-chan (listen el :mousedown)
+        move-chan (listen el :mousemove)
+        end-chan (listen el :mouseup)]
+    (go (loop [engaged? false moved? false]
+          (let [[v c] (alts! [start-chan move-chan end-chan])]
+            (condp = c
+              start-chan (if (start-predicate v) 
+                           (do 
+                             (>! out {:type :start :x (.-clientX v) :y (.-clientY v)})
+                             (recur true false))
+                           (recur false false))
+              end-chan (do 
+                         (when (and moved? engaged?)
+                           (>! out {:type :end :x (.-clientX v) :y (.-clientY v)}))
+                         (recur false false))
+              move-chan (do
+                          (when engaged? (>! out {:type :move :x (.-clientX v) :y (.-clientY v)}))
+                          (recur engaged? true)))
+            (close! out))))
+    out))
+
 
 ;;; Model
 
@@ -20,7 +68,6 @@
                                           :image-view {:type "image-view" :src "../img/index.jpe"}
                                           :list-view {:type "list-view" :class "centered"
                                                       :items [{:title "foo"} {:title "bar"} {:title "baz"}]}}}
-                      :show-menu? false
                       :menu {:items [{:title "page1" :href "#pages/title-view"
                                       :class "side-menu-list-item"}
                                      {:title "page2" :href "#pages/image-view"
@@ -80,22 +127,81 @@
 
 (defn app [app-state owner]
   (reify
-    om/IRender
-    (render [this]
-      (let [show-menu? (:show-menu? app-state)
-            show-menu-class (if show-menu? "show-menu" "hide-menu")]
-        (dom/div #js{:className "full-height"} 
-                 (dom/div #js{:id "main-page-wrapper" :className show-menu-class}
-                          (dom/div #js{:id "top-bar"}
-                                   (dom/button #js{:id "hamburger-btn"
-                                                   :onClick #(om/update! app-state :show-menu? (not show-menu?))} 
-                                               "\u2630"))
-                          (om/build main-page (:main-page app-state))
-                          (let [class "main-page-overlay"]
-                            (dom/div #js{:className (if show-menu? class (str class " hide")) 
-                                         :onClick #(om/update! app-state :show-menu? (not show-menu?))})))
-                 (dom/div #js{:id "left-slide-menu" :className show-menu-class} 
-                          (om/build menu (:menu app-state))))))))
+    om/IInitState
+    (init-state [this] 
+      {:menu-on? false 
+       :menu-chan (chan)
+       :resize-chan (chan)
+       :menu-x-pos: 0
+       :menu-width: 0
+       :drag-start-point 0
+       :drag-events (drag js/document
+                          (fn [e] 
+                            (let [x (.-clientX e)
+                                  menu-width (om/get-state owner :menu-width)
+                                  x-pos (om/get-state owner :menu-x-pos)]
+                              (if (= x-pos 0)
+                                (and (> x 0) (< x (+ x-pos menu-width)))
+                                (and (> x 0) (< x 20))))))})
+
+    om/IWillMount
+    (will-mount [_]
+      (let [menu-chan (om/get-state owner :menu-chan)
+            drag-chan (om/get-state owner :drag-events)
+            resize-chan (om/get-state owner :resize-chan)]
+        (go (loop []
+              (if (= (<! menu-chan) true)
+                (om/set-state! owner :menu-x-pos 0)
+                (om/set-state! owner :menu-x-pos (- (om/get-state owner :menu-width))))
+              (recur)))
+        (go (loop []
+              (let [drag-event (<! drag-chan)
+                    x (:x drag-event)
+                    old-x (om/get-state owner :menu-x-pos)
+                    menu-width (om/get-state owner :menu-width)]
+                (condp = (:type drag-event)
+                  :start (om/set-state! owner :drag-start-point (.abs js/Math (- old-x (- x menu-width))))
+                  :end (do
+                         (om/set-state! owner :drag-start-point 0)
+                         (if (< x (/ menu-width 2)) 
+                           (>! menu-chan false)
+                           (>! menu-chan true)))
+                  (when (< (+ x (om/get-state owner :drag-start-point)) menu-width)
+                      (om/set-state! owner :menu-x-pos (+ 
+                                                        (- x menu-width)
+                                                        (om/get-state owner :drag-start-point))))))
+              (recur)))
+        (go (loop []
+              (let [resize-event (<! resize-chan)
+                    new-width (.-offsetWidth (om/get-node owner "slide-menu"))]
+                (om/set-state! owner :menu-width new-width))
+              (recur)))))
+    
+    om/IDidMount
+    (did-mount [this]
+      (let [menu-width (.-offsetWidth (om/get-node owner "slide-menu"))]
+        (om/set-state! owner :menu-width menu-width)
+        (om/set-state! owner :menu-x-pos (- menu-width))
+        (listen js/window :resize nil (om/get-state owner :resize-chan))))
+
+    om/IRenderState
+    (render-state [this {:keys [menu-chan menu-on? menu-x-pos]}]
+      (dom/div #js{:className "full-height"}
+               (dom/div #js{:id "main-page-wrapper" :className (if menu-on? "menu-on" "menu-off")}
+                        (dom/div #js{:id "top-bar"}
+                                 (dom/button #js{:id "hamburger-btn"
+                                                 :onClick #(put! menu-chan true)} 
+                                             "\u2630"))
+                        (om/build main-page (:main-page app-state))
+                        (let [class "main-page-overlay"]
+                          (dom/div #js{:className (if menu-on? class (str class " hide")) 
+                                       :onClick #(put! menu-chan false)})))
+               (dom/div #js{:id "left-slide-menu" 
+                            :style #js{:transform (str "translate3d(" menu-x-pos "px, 0, 0)")}
+                            :ref "slide-menu"} 
+                        (om/build menu (:menu app-state)))))))
+
+(def resize (listen js/window :resize))
 
 (om/root app app-state
          {:target (. js/document (getElementById "app")) })
@@ -108,7 +214,7 @@
   (swap! app-state update-in [:main-page] assoc :current-page (keyword id)))
 
 (let [h (History.)]
-  (goog.events/listen h EventType/NAVIGATE #(secretary/dispatch! (.-token %)))
+  (goog.events/listen h HistoryEventType/NAVIGATE #(secretary/dispatch! (.-token %)))
   (doto h (.setEnabled true)))
 
 ;;; Figwheel
